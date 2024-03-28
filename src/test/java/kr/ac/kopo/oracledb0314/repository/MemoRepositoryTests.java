4d4a646ba195e7cb260c69cd30a54d2b5a49b520
Leegongseok <seok20108014@gmail.com>
2024-03-28 09:45:28 +0900

    페이지처리2

--- src/test/java/kr/ac/kopo/oracledb0314/repository/MemoRepositoryTests.java
+++ src/test/java/kr/ac/kopo/oracledb0314/repository/MemoRepositoryTests.java
@@ -7,8 +7,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.SQLOutput;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -17,78 +19,103 @@ public class MemoRepositoryTests {
 
     @Autowired
     MemoRepository memoRepository;
+
     @Test
-    public void testClass(){
+    public void testClass() {
+
         System.out.println(memoRepository.getClass().getName());
     }
-// MemoRepository의 save(Memo Entity객체의 참조값)를 호출해서 insert한다
+
+    // MemoRepository의 save(Memo Entity객체의 참조값)를 호출해서 insert한다
     @Test
-    public void testInsertDummies(){
-        IntStream.rangeClosed(1,100).forEach(i->{
-            Memo memo=Memo.builder().memoText("Dummy Data test"+i).build();
+    public void testInsertDummies() {
+        IntStream.rangeClosed(1, 100).forEach(i -> {
+            Memo memo = Memo.builder().memoText("Dummy Data test" + i).build();
             memoRepository.save(memo);
         });
     }
-// MemoRepository의 findById(Memo Entity 객체의 Id로 설정된 필드값)를 호출해서 select한다
+
+    // MemoRepository의 findById(Memo Entity 객체의 Id로 설정된 필드값)를 호출해서 select한다
 //  findById()호출되면 바로 select문을 실행한다
 //  findById()는 NullpointException이 발생되지 않도록 Null체크를 한다
     @Test
-    public void testselect(){
-        Long mno= 100l;
+    public void testselect() {
+        Long mno = 100l;
 
-        Optional<Memo> result= memoRepository.findById(mno);
+        Optional<Memo> result = memoRepository.findById(mno);
 
         System.out.println("============================================================");
 
-        if(result.isPresent()) {
+        if (result.isPresent()) {
             Memo memo = result.get();
             System.out.println(memo);
         }
     }
-//MemoRepository의 getOne(Memo Entity 객체의 Id로 설정된 필드값)를 호출해서 select한다
+
+    //MemoRepository의 getOne(Memo Entity 객체의 Id로 설정된 필드값)를 호출해서 select한다
     //getone은 호출되면 바로 실행되지 않고 Memo Entity가 필요할때 select를 실행한다
     @Transactional
     @Test
-    public void testselect2(){
-    Long mno= 100l;
+    public void testselect2() {
+        Long mno = 100l;
 
-    Memo memo= memoRepository.getOne(mno);
+        Memo memo = memoRepository.getOne(mno);
 
-    System.out.println("============================================================");
+        System.out.println("============================================================");
 
 
-    System.out.println(memo);
+        System.out.println(memo);
     }
+
     // MemoRepository의 save(Memo Entity객체의 참조값)를 호출해서 Update한다
     // save()는 호출되면 먼저 select를 하기 때문에 기존에 Entity가 있을때는 update를 실행한다.
     @Test
-    public void testUpdate(){
-        Memo memo =Memo.builder().mno(95l).memoText("Update Dummy Data").build();
+    public void testUpdate() {
+        Memo memo = Memo.builder().mno(95l).memoText("Update Dummy Data").build();
 
-        Memo memo1=memoRepository.save(memo);
+        Memo memo1 = memoRepository.save(memo);
 
         System.out.println(memo1);
     }
+
     // MemoRepository의 deleteById(Memo Entity의 mno)를 호출해서 delete한다
     @Test
-    public void testDelete(){
+    public void testDelete() {
 
-        Long mno=100L;
+        Long mno = 100L;
 
         memoRepository.deleteById(mno);
     }
-    @Test
-    public void testPageDefault(){
-        //1페이지당 10개 Entity
-        Pageable pageable = PageRequest.of(1,10);
 
-        Page<Memo> result=memoRepository.findAll(pageable);
+    @Test
+    public void testPageDefault() {
+        //1페이지당 10개 Entity
+        Pageable pageable = PageRequest.of(1, 10);
+
+        Page<Memo> result = memoRepository.findAll(pageable);
 
         System.out.println(result);
 
-        for(Memo memo : result.getContent()){
+        for (Memo memo : result.getContent()) {
             System.out.println(memo);
         }
-    }
+        System.out.println("====================================");
+        System.out.println("Total pages:"+result.getTotalPages());
+        System.out.println("Total Count:"+result.getTotalElements());
+        System.out.println("Total Number:"+result.getNumber());
+        System.out.println("page Size:"+result.getSize());
+        System.out.println("Has next page?:"+result.hasNext());
+        System.out.println("Is first page?:"+result.isFirst());
 
+
+    }
+    @Test
+    public void testSort(){
+        Pageable pageable=PageRequest.of(0,10, Sort.by("mno").descending());
+        Page<Memo>result=memoRepository.findAll(pageable);
+
+        result.get().forEach(memo -> {
+            System.out.println("number:"+memo.getMno()+",content"+memo.getMemoText());
+        });
+    }
 }//클래스 끝
